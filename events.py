25ae2431bca3369feee45ed93d5621f2348a1f9c
ramyashah27 <75496156+ramyashah27@users.noreply.github.com>
2021-01-05 22:24:16 +0530

    Add files via upload

---
+++ events.py
@@ -0,0 +1,10 @@
+# import pygame
+# gameWindow = pygame.display.set_mode((1200,500))
+# pygame.display.set_caption('FIRST PROJE CT')
+# exit_game= False
+# game_over = False
+# while not exit_game:
+#     for event in pygame.event.get():
+#         print(event)
+# pygame.quit()
+# quit()
---
+++ givingtitle.py
@@ -0,0 +1,5 @@
+import pygame
+gameWindow = pygame.display.set_mode((1200,500))
+pygame.display.set_caption('FIRST PROJE CT')
+exit_game= False
+game_over = False
